6998dffc963683a378fc5516d990c61c2fce8f46
JCBirelo <jc.birelo7@gmail.com>
2025-05-16 07:44:25 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import FormularioEtapas from "../components/FormularioEtapas";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold text-center mb-6">Formulário Prefeitura</h1>
+      <FormularioEtapas />
+    </div>
+  );
+}
+
+export default App;
